c369eaca24c3e8f88f912777f4e563466cfae3ac
Endyanto Dwi Saputro <screamolic@users.noreply.github.com>
2021-08-20 21:58:51 +0700

    Create installer.sh

---
+++ installer.sh
@@ -0,0 +1,34 @@
+#!/bin/bash
+clear
+# Checking permissions
+if [[ $EUID -ne 0 ]]; then
+   ee_lib_echo_fail "Sudo privilege required..."
+   exit 100
+fi
+
+# Define echo function
+# Blue color
+function ee_lib_echo()
+{
+   echo $(tput setaf 4)$@$(tput sgr0)
+}
+# White color
+function ee_lib_echo_info()
+{
+   echo $(tput setaf 7)$@$(tput sgr0)
+}
+# Red color
+function ee_lib_echo_fail()
+{
+   echo $(tput setaf 1)$@$(tput sgr0)
+}
+
+# Execute: update
+ee_lib_echo "Updating, please wait..."
+yum -y update > echo null
+
+dnf install https://dl.fedoraproject.org/pub/epel/epel-release-latest-8.noarch.rpm -y
+dnf install https://rpms.remirepo.net/enterprise/remi-release-8.rpm -y
+
+dnf module enable php:remi-7.4
+dnf install php php-cli php-common php-devel -y
